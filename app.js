d769fb31ffa2d506fe39314c4f23d24ef8183478
java01627 <navymeester@gmail.com>
2021-06-19 14:13:30 +0800

    app.js

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const numberImput =document.getElementById('number');
 const btn =document.getElementById('btn');
-const randomNumber = Math.floor(Math.random()*6)+1
+const randomNumber = Math.floor(Math.random()*6)+1;
 
-btn.addEventListener('click'),function
+btn.addEventListener('click',function(){
+    const userVal = parseInt(numberInput.value,10);
+    
+    if(userVal===randomNumber){
+        alert('You are Right');
+        return;
+    }
+    
+    if(useVal>randomNumber){
+        alert('smaller');
+        return;
+    }
+    if(userVal<randomNumber){
+        alert('large');
+    
+    }
+});
