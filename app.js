4fd3a00138924d6c81fa8bdad3094f1c498f337b
java01627 <navymeester@gmail.com>
2021-06-19 14:00:48 +0800

    app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+const numberImput =document.getElementById('number');
+const btn =document.getElementById('btn');
+const randomNumber = Math.floor(Math.random()*6)+1
+
+btn.addEventListener('click'),function
